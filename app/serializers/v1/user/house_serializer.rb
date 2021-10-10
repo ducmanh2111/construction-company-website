e08de56b0ce0895660af0d235c4725456b912908
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2021-10-10 09:34:52 +0700

    222: [API] Chức năng index, show cho House

---
+++ app/serializers/v1/user/house_serializer.rb
@@ -0,0 +1,8 @@
+module V1
+  module User
+    class HouseSerializer < ActiveModel::Serializer
+      attributes :id, :house_category_id, :house_category_name, :name, :address, :floors,
+                :year, :country, :status, :description, :area
+    end
+  end
+end
