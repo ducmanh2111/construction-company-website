62cab4682ac9976e0b81520920ca856b40c054aa
cminh <cminh@cminhs-MacBook-Pro.local>
2021-10-24 11:21:57 +0700

    change navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import List from "./pages/List";
+import cateApi from "./service.api/category";
+import roomApi from "./service.api/room";
 
 function App() {
+
+    const [categories, setCategories] = useState([]);
+    const [roomCategories, setRoomCategories] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const fetchedCategories = await cateApi.getAll()
+                setCategories(fetchedCategories.data.house_categories)
+                const fetchedroomCategories = await roomApi.getAll()
+                setRoomCategories(fetchedroomCategories.data.room_categories)
+            } catch (error) {
+                console.log(error);
+            }
+            
+        }
+        fetchData()
+    }, [])
+
+
     return (
         <>
-            <Navbar />
+            <Navbar categories={categories} roomCategories={roomCategories}/>
             <div className="page-wrapper">
                 <Switch>
                     <Route exact path="/home" component={Home} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,100 +1,93 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
-    return (
-        <nav className="activ">
-            {/* logo */}
-            <div className="logo-box">
-                <a href="index.html" className="logo logo-img">
-                    <img src="assets/img/logo.png" alt="logo" />
-                </a>
-            </div>
-            {/* mobile */}
-            <button
-                type="button"
-                aria-label="Toggle Navigation"
-                className="menu-button-mobile lines-button x2"
-            >
-                <span className="lines" />
-            </button>
-            {/* Menu */}
-            <div
-                className="menu-box my-scrollbar"
-                style={{ maxHeight: "467px" }}
-            >
-                <div id="dl-menu" className="dl-menuwrapper">
-                    <ul className="dl-menu dl-menuopen">
-                        <li>
-                            <Link to="/home">home</Link>
-                        </li>
-                        <li>
-                            <a href="about.html">about</a>
-                        </li>
-                        <li>
-                            <a href="contact.html">contact</a>
-                        </li>
-                        <li className="submenu">
-                            <a href="#">House</a>
-                            <ul className="sub-menu">
-                                <li>
-                                    <Link to="/list">Villa</Link>
-                                </li>
-                                <li>
-                                    <Link to="/list">Tube House</Link>
-                                </li>
-                                <li>
-                                    <Link to="/list">Apartment</Link>
-                                </li>
-                            </ul>
-                        </li>
-                        <li className="submenu">
-                            <a href="#">Room</a>
-                            <ul className="sub-menu">
-                                <li>
-                                    <Link to="/list">Bedroom</Link>
-                                </li>
-                                <li>
-                                    <Link to="/list">Kitchen</Link>
-                                </li>
-                                <li>
-                                    <Link to="/list">Toilet</Link>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            {/* End Menu */}
-            <footer className="footer" style={{}}>
-                <ul className="social-icons">
-                    <li>
-                        <Link to="#" target="_blank">
-                            <i className="fa fa-facebook" />
-                        </Link>
+const Navbar = (props) => {
+  const { categories, roomCategories } = props;
+
+  return (
+    <nav className="activ">
+      {/* logo */}
+      <div className="logo-box">
+        <a href="index.html" className="logo logo-img">
+          <img src="assets/img/logo.png" alt="logo" />
+        </a>
+      </div>
+      {/* mobile */}
+      <button
+        type="button"
+        aria-label="Toggle Navigation"
+        className="menu-button-mobile lines-button x2"
+      >
+        <span className="lines" />
+      </button>
+      {/* Menu */}
+      <div className="menu-box my-scrollbar" style={{ maxHeight: "467px" }}>
+        <div id="dl-menu" className="dl-menuwrapper">
+          <ul className="dl-menu dl-menuopen">
+            <li>
+              <Link to="/home">home</Link>
+            </li>
+            <li>
+              <a href="about.html">about</a>
+            </li>
+            <li>
+              <a href="contact.html">contact</a>
+            </li>
+            <li className="submenu">
+              <a href="#">Nhà</a>
+              <ul className="sub-menu">
+                {categories &&
+                  categories.map((cat) => (
+                    <li key={cat.id}>
+                      <Link to="/list">{cat.name}</Link>
                     </li>
-                    <li>
-                        <Link to="#" target="_blank">
-                            <i className="fa fa-twitter" />
-                        </Link>
+                  ))}
+              </ul>
+            </li>
+            <li className="submenu">
+              <a href="#">Phòng</a>
+              <ul className="sub-menu">
+              {roomCategories &&
+                  roomCategories.map((room) => (
+                    <li key={room.id}>
+                      <Link to="/list">{room.name}</Link>
                     </li>
-                    <li>
-                        <Link to="#" target="_blank">
-                            <i className="fa fa-pinterest" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#" target="_blank">
-                            <i className="fa fa-yelp" />
-                        </Link>
-                    </li>
-                </ul>
-                {/* <div className="copyright">
+                  ))}
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* End Menu */}
+      <footer className="footer" style={{}}>
+        <ul className="social-icons">
+          <li>
+            <Link to="#" target="_blank">
+              <i className="fa fa-facebook" />
+            </Link>
+          </li>
+          <li>
+            <Link to="#" target="_blank">
+              <i className="fa fa-twitter" />
+            </Link>
+          </li>
+          <li>
+            <Link to="#" target="_blank">
+              <i className="fa fa-pinterest" />
+            </Link>
+          </li>
+          <li>
+            <Link to="#" target="_blank">
+              <i className="fa fa-yelp" />
+            </Link>
+          </li>
+        </ul>
+        {/* <div className="copyright">
                     © 2021 <Link to="#"></Link>
                 </div> */}
-            </footer>
-        </nav>
-    );
+      </footer>
+    </nav>
+  );
 };
 
 export default Navbar;
