8edb56c6791e25a784632e17b54f17c79c158c5d
t-manh <t-manh@usernoMacBook-puro.local>
2021-10-24 15:34:38 +0700

    hot_fix-adding-house_name-and-room_category_name

--- backend/app/serializers/v1/user/room_serializer.rb
+++ backend/app/serializers/v1/user/room_serializer.rb
@@ -1,7 +1,7 @@
 module V1
   module User
     class RoomSerializer < ActiveModel::Serializer
-      attributes :id, :house_id, :area, :door, :window, :door_direction
+      attributes :id, :house_id, :area, :door, :window, :door_direction, :room_category_name, :house_name
     end
   end
 end
