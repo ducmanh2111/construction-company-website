f9f9fa095c6cffb823a6eedcabfc425c0db3334e
manhtd <tdm@gmail.com>
2021-11-02 02:03:01 +0700

    245: [Admin] Implement EditRoom screen

--- frontend-admin/src/components/ListRoom/NewRoom.js
+++ frontend-admin/src/components/ListRoom/NewRoom.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Col, Row, Button, FloatingLabel } from 'react-bootstrap';
+import { Form, Col, Row, Button, FloatingLabel, Container } from 'react-bootstrap';
 import { forEach } from 'lodash';
 import roomApi from '../../services/roomApi';
 import roomCategoryApi from '../../services/roomCategoryApi';
 import houseApi from '../../services/houseApi';
 
-
 export default function NewRoom() {
-
   const [roomCategories, setRoomCategories] = useState([]);
   const [roomCategoryId, setRoomCategoryId] = useState(1);
   const [houses, setHouses] = useState([]);
@@ -65,78 +63,80 @@ export default function NewRoom() {
   }
 
   return (
-    <Form onSubmit={submitForm}>
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridRoomType">
-          <Form.Label>Room Type</Form.Label>
-          <Form.Select defaultValue="1" onChange={(e) => setRoomCategoryId(e.target.value)}>
-            {roomCategories.map(room_category => {
-              return (<option value={room_category.id} key={room_category.id}> {room_category.name} </option>);
-            })}
-          </Form.Select>
-        </Form.Group>
+    <Container className="mt-3">
+      <Form onSubmit={submitForm}>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridRoomType">
+            <Form.Label>Room Type</Form.Label>
+            <Form.Select defaultValue="1" onChange={(e) => setRoomCategoryId(e.target.value)}>
+              {roomCategories.map(room_category => {
+                return (<option value={room_category.id} key={room_category.id}> {room_category.name} </option>);
+              })}
+            </Form.Select>
+          </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridHouseName">
-          <Form.Label>House Name</Form.Label>
-          <Form.Select defaultValue="1" onChange={(e) => setHouseId(e.target.value)}>
-            {houses.map(house => {
-              return (<option value={house.id} key={house.id}> {house.name} </option>);
-            })}
-          </Form.Select>
-        </Form.Group>
-      </Row>
-      
-      <Row className="mb-3">
-        <h3>Detail information</h3>
-        <FloatingLabel
-          controlId="floatingInputArea"
-          label="Area"
-          className="mb-3"
-          as={Col}
-        >
-          <Form.Control placeholder="Area" onChange={(e) => setArea(e.target.value)}/>
-        </FloatingLabel>
+          <Form.Group as={Col} controlId="formGridHouseName">
+            <Form.Label>House Name</Form.Label>
+            <Form.Select defaultValue="1" onChange={(e) => setHouseId(e.target.value)}>
+              {houses.map(house => {
+                return (<option value={house.id} key={house.id}> {house.name} </option>);
+              })}
+            </Form.Select>
+          </Form.Group>
+        </Row>
 
-        <FloatingLabel
-          controlId="floatingInputWindow"
-          label="Number of Windows"
-          className="mb-3"
-          as={Col}
-        >
-          <Form.Control placeholder="Window" onChange={(e) => setWindows(e.target.value)}/>
-        </FloatingLabel>
+        <Row className="mb-3">
+          <h3>Detail information</h3>
+          <FloatingLabel
+            controlId="floatingInputArea"
+            label="Area"
+            className="mb-3"
+            as={Col}
+          >
+            <Form.Control placeholder="Area" onChange={(e) => setArea(e.target.value)}/>
+          </FloatingLabel>
 
-        <FloatingLabel
-          controlId="floatingInputDoor"
-          label="Number of Doors"
-          className="mb-3"
-          as={Col}
-        >
-          <Form.Control placeholder="Door" onChange={(e) => setDoor(e.target.value)}/>
-        </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingInputWindow"
+            label="Number of Windows"
+            className="mb-3"
+            as={Col}
+          >
+            <Form.Control placeholder="Window" onChange={(e) => setWindows(e.target.value)}/>
+          </FloatingLabel>
+
+          <FloatingLabel
+            controlId="floatingInputDoor"
+            label="Number of Doors"
+            className="mb-3"
+            as={Col}
+          >
+            <Form.Control placeholder="Door" onChange={(e) => setDoor(e.target.value)}/>
+          </FloatingLabel>
 
 
-        <FloatingLabel controlId="floatingInputDoorDirection" label="Door direction" as={Col}>
-          <Form.Select aria-label="Door direction" onChange={(e) => setDoorDirection(e.target.value)} className="mb-3">
-            <option>Please select</option>
-            <option value="East">Đông</option>
-            <option value="West">Tây</option>
-            <option value="South">Nam</option>
-            <option value="North">Bắc</option>
-          </Form.Select>
-        </FloatingLabel>
-      </Row>
-      
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formFileMultiple" className="mb-3">
-          <Form.Label>House Image</Form.Label>
-          <Form.Control type="file" multiple onChange={(e) => handleFile(e)}/>
-        </Form.Group>
-      </Row>
+          <FloatingLabel controlId="floatingInputDoorDirection" label="Door direction" as={Col}>
+            <Form.Select aria-label="Door direction" onChange={(e) => setDoorDirection(e.target.value)} className="mb-3">
+              <option>Please select</option>
+              <option value="East">Đông</option>
+              <option value="West">Tây</option>
+              <option value="South">Nam</option>
+              <option value="North">Bắc</option>
+            </Form.Select>
+          </FloatingLabel>
+        </Row>
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formFileMultiple" className="mb-3">
+            <Form.Label>House Image</Form.Label>
+            <Form.Control type="file" multiple onChange={(e) => handleFile(e)}/>
+          </Form.Group>
+        </Row>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 }
--- frontend-admin/src/services/roomApi.js
+++ frontend-admin/src/services/roomApi.js
@@ -18,7 +18,19 @@ const roomApi = {
     });
   },
   get: (id) => {
-    return axios.get(`/api/v1/user/rooms/${id}`).then(res => res.data.room);
+    return axios.get(`/api/v1/rooms/${id}`).then(res => res.data.room);
+  },
+  patch: (id, params) => {
+    return axios.patch(`/api/v1/rooms/${id}`, params, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+    }).then(function (response) {
+      return response;
+    })
+    .catch(function (error) {
+      throw error;
+    });
   }
 }
 
